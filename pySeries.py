0694c582d0301336f46785aa25cd83c38fbf0f1c
jameslee5656 <james.lee56566@gmail.com>
2018-10-03 19:03:05 +0800

    Change 2018-10-3 Add pySeries

---
+++ pySeries.py
@@ -0,0 +1,20 @@
+import serial
+import time
+import codecs
+import time
+
+ser=serial.Serial("COM9", 9600, timeout= 0.5 ) 
+ser.write(bytes("AT+NSOCR=DGRAM,17,8888,1\r\n", 'UTF-8'))
+ser.write(bytes("AT+NPING=120.126.136.17\r\n", 'UTF-8'))
+SendtoServer = 'AT+NSOST=1,120.126.136.17,5687'
+for count in range(1,1000):   
+    message = str(time.time())
+    messageHexAscii = ''
+    message = str(count) + ' ' + message  
+    messageByteNumber = str(len(message.encode('utf-8')))
+    for x in message:
+        messageHexAscii += format(ord(x), "x")
+    if ser.is_open:
+        ser.write(bytes(SendtoServer + ',' + messageByteNumber + ','
+                + messageHexAscii + '\r\n', 'UTF-8'))
+    time.sleep(5)
