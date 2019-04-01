51d51357b573ed1c97fa56d04e90725501251d43
jameslee5656 <jameslee56566@gmail.com>
2019-04-01 22:35:43 +0800

    2019-4-1 start data analyzing

---
+++ Database/autobackup.py
@@ -0,0 +1,30 @@
+import paramiko
+import datetime
+import os
+import time
+
+REMOTEHOST = '120.126.136.17'
+USERNAME = 'bigdata'
+KEYPATH = '/home/james/.ssh/id_rsa'
+
+#ssh settings
+ssh = paramiko.SSHClient()
+key = paramiko.RSAKey.from_private_key_file(KEYPATH)
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(hostname=REMOTEHOST, port=22,username=USERNAME, pkey=key)
+now = datetime.datetime.now()
+firstflag = True
+while True: 
+	if firstflag or int((datetime.datetime.now() - now).total_seconds()) > 60*60*12:
+		now = datetime.datetime.now()
+		filename = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S") 
+		#code, printout
+		ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command \
+						('mongodump -h 127.0.0.1 -d Tracker -o /home/bigdata/dump/'+ filename)
+		print('\n'.join( ssh_stdout.readlines()))
+
+		os.system("scp -r bigdata@120.126.136.17:/home/bigdata/dump/" + filename + " .")
+		os.system("mongorestore -h 127.0.0.1 -d Tracker " + filename + "/Tracker --drop")
+		firstflag = False
+	else:
+		time.sleep(60*61)
