c69b565c770771466d17ad973929a08f54e98971
jameslee5656 <jameslee56566@gmail.com>
2019-05-10 10:39:49 +0800

    2019-5-10 update setfence

--- Tracker3/SetFence1.3.py
+++ Tracker3/SetFence1.3.py
@@ -75,130 +75,130 @@ class electricFence():
         # squarefreq = df.groupby(df[['longitude','latitude']].columns.tolist(),as_index=False).size()
         squarefreq = {}
         #make out a dic that is the smallest boundScale = 1
-        for i in range(int(round(0.02,4)*10000)):
-            for j in range(int(round(0.02,4)*10000)):
+        for i in range(int(round(distance * 2,4)*10000)):
+            for j in range(int(round(distance * 2,4)*10000)):
                 squarefreq[tuple([round(downlat + i/10000 ,4), round(leftlong + j/10000,4)])] = 0
-        print(squarefreq)
-#         # print(temptlist)
-#         for x in temptlist:
-#             # print(tuple(x), tuple(x) in squarefreq)
-#             if tuple(x) in squarefreq:
-#                 #the way i think: use mod to divide into two group just like roundup
-#                 if round((x[0] - downlat)*10000,0) % boundScale != 0:
-#                     #less or equal than boundScale/2 = the smaller on 
-#                     if round((x[0] - downlat)*10000,0)% boundScale <= boundScale/2:
-#                         x[0] -= (round((x[0] - downlat)*10000,0) % boundScale)/10000
-#                     else:
-#                         x[0] += (boundScale - (round((x[0] - downlat)*10000,0) % boundScale))
-#                     x[0] = round(x[0],4)
-#                 if round((x[1] - leftlong)*10000,0) % boundScale != 0:
-#                     if round((x[0] - leftlong)*10000,0) % boundScale != 0:
-#                         x[1] -= (round((x[0] - leftlong)*10000,0) % boundScale)/10000
-#                     else:
-#                         x[1] += (boundScale - (round((x[0] - leftlong)*10000,0) % boundScale))
-#                     x[1] = round(x[1],4)
-#                 if tuple(x) in squarefreq:
-#                     squarefreq[tuple(x)] += 1
+        # print(squarefreq)
+        # print(temptlist)
+        temptlist = self.df[['latitude','longitude']].values
+        for x in temptlist:
+            # print(tuple(x), tuple(x) in squarefreq)
+            if tuple(x) in squarefreq:
+                #the way i think: use mod to divide into two group just like roundup
+                if round((x[0] - downlat)*10000,0) % boundScale != 0:
+                    #less or equal than boundScale/2 = the smaller on 
+                    if round((x[0] - downlat)*10000,0)% boundScale <= boundScale/2:
+                        x[0] -= (round((x[0] - downlat)*10000,0) % boundScale)/10000
+                    else:
+                        x[0] += (boundScale - (round((x[0] - downlat)*10000,0) % boundScale))
+                    x[0] = round(x[0],4)
+                if round((x[1] - leftlong)*10000,0) % boundScale != 0:
+                    if round((x[0] - leftlong)*10000,0) % boundScale != 0:
+                        x[1] -= (round((x[0] - leftlong)*10000,0) % boundScale)/10000
+                    else:
+                        x[1] += (boundScale - (round((x[0] - leftlong)*10000,0) % boundScale))
+                    x[1] = round(x[1],4)
+                if tuple(x) in squarefreq:
+                    squarefreq[tuple(x)] += 1
 
 #         #divide into different percentage
-#         boundlist = []
+        boundlist = []
 #         # print(squarefreq)
-#         squarefreqMax = max(squarefreq.values())
+        squarefreqMax = max(squarefreq.values())
 #         # print(squarefreqMax)
-#         if squarefreqMax != 0:
-#             squarefreqMax = math.log10(max(squarefreq.values()))
-#         for k,values in squarefreq.items():
-#             key = list(k)
-#             org_values = values
-#             # print(org_values)
-#             if round((key[0] - downlat)*10000,0) % boundScale == 0:
-#                 if round((key[1] - leftlong)*10000,0) % boundScale == 0:
-#                     lt = [round(key[0] + 0.00005*boundScale,5), round(key[1] - 0.00005*boundScale,5)]
-#                     rt = [round(key[0] + 0.00005*boundScale,5), round(key[1] + 0.00005*boundScale,5)]
-#                     rd = [round(key[0] - 0.00005*boundScale,5), round(key[1] + 0.00005*boundScale,5)]
-#                     ld = [round(key[0] - 0.00005*boundScale,5), round(key[1] - 0.00005*boundScale,5)]
-#                     precentage = 0
-#                     if values!=0:
-#                         values = math.log10(values)
-#                     if(values>=0)and(values<squarefreqMax/5):
-#                         precentage = 0
-#                     elif (values>=squarefreqMax/5)and(values<2*squarefreqMax/5):
-#                         precentage = 1
-#                     elif (values>=2*squarefreqMax/5)and(values<3*squarefreqMax/5):
-#                         precentage = 2
-#                     elif (values>=3*squarefreqMax/5)and(values<4*squarefreqMax/5):
-#                         precentage = 3
-#                     elif (squarefreqMax == 0):
-#                         precentage = 0
-#                     else:
-#                         precentage = 4
-#                     boundlist.append([lt,rt,rd,ld,precentage,org_values])
+        if squarefreqMax != 0:
+            squarefreqMax = math.log10(max(squarefreq.values()))
+        for k,values in squarefreq.items():
+            key = list(k)
+            org_values = values
+            # print(org_values)
+            if round((key[0] - downlat)*10000,0) % boundScale == 0:
+                if round((key[1] - leftlong)*10000,0) % boundScale == 0:
+                    lt = [round(key[0] + 0.00005*boundScale,5), round(key[1] - 0.00005*boundScale,5)]
+                    rt = [round(key[0] + 0.00005*boundScale,5), round(key[1] + 0.00005*boundScale,5)]
+                    rd = [round(key[0] - 0.00005*boundScale,5), round(key[1] + 0.00005*boundScale,5)]
+                    ld = [round(key[0] - 0.00005*boundScale,5), round(key[1] - 0.00005*boundScale,5)]
+                    precentage = 0
+                    if values!=0:
+                        values = math.log10(values)
+                    if(values>=0)and(values<squarefreqMax/5):
+                        precentage = 0
+                    elif (values>=squarefreqMax/5)and(values<2*squarefreqMax/5):
+                        precentage = 1
+                    elif (values>=2*squarefreqMax/5)and(values<3*squarefreqMax/5):
+                        precentage = 2
+                    elif (values>=3*squarefreqMax/5)and(values<4*squarefreqMax/5):
+                        precentage = 3
+                    elif (squarefreqMax == 0):
+                        precentage = 0
+                    else:
+                        precentage = 4
+                    boundlist.append([lt,rt,rd,ld,precentage,org_values])
                     
-#         squarelen = int(math.sqrt(len(boundlist)))
-#         # print(squarelen,'\n\n////')
-#         spacelist = []
-#         #imply from bottomleft to topright
-#         # temptlist.append(boundlist[0][3])
-#         # temptlist.append(boundlist[0][2])
-#         # temptlist.append(boundlist[0][0])
-#         # temptlist.append(boundlist[0][1])
+        squarelen = int(math.sqrt(len(boundlist)))
+        # print(squarelen,'\n\n////')
+        spacelist = []
+        #imply from bottomleft to topright
+        # temptlist.append(boundlist[0][3])
+        # temptlist.append(boundlist[0][2])
+        # temptlist.append(boundlist[0][0])
+        # temptlist.append(boundlist[0][1])
 
-#         # temptlist.append(boundlist[0][3])
-#         # temptlist.append(boundlist[10][1])
-#         # temptlist.append(boundlist[1][3])
-#         # temptlist.append([boundlist[10][1][0],boundlist[1][3][1]])
+        # temptlist.append(boundlist[0][3])
+        # temptlist.append(boundlist[10][1])
+        # temptlist.append(boundlist[1][3])
+        # temptlist.append([boundlist[10][1][0],boundlist[1][3][1]])
 
-#         #imply from topright to bottomleft
-#         # temptlist.append(boundlist[squarelen*squarelen - 1][1])
-#         # temptlist.append(boundlist[squarelen*squarelen - 1][3])
-#         # temptlist.append(boundlist[squarelen*squarelen - 1][2])
-#         # temptlist.append(boundlist[squarelen*squarelen - 1][0])
+        #imply from topright to bottomleft
+        # temptlist.append(boundlist[squarelen*squarelen - 1][1])
+        # temptlist.append(boundlist[squarelen*squarelen - 1][3])
+        # temptlist.append(boundlist[squarelen*squarelen - 1][2])
+        # temptlist.append(boundlist[squarelen*squarelen - 1][0])
 
-#         # temptlist.append(boundlist[squarelen*squarelen - 2][0])
-#         # temptlist.append(boundlist[squarelen*(squarelen-1) - 1][2])
-#         # temptlist.append([boundlist[squarelen*(squarelen-1) - 1][3][0],boundlist[squarelen*squarelen - 2][0][1]])
-#         for x in range(squarelen):
-#             temptlist = []
-#             temptlist.append(boundlist[0][3])
-#             temptlist.append(boundlist[x * squarelen][0])
-#             temptlist.append([boundlist[x * squarelen][0][0],boundlist[x][2][1]])
-#             temptlist.append(boundlist[x][2])
-#             spacelist.append(temptlist)
+        # temptlist.append(boundlist[squarelen*squarelen - 2][0])
+        # temptlist.append(boundlist[squarelen*(squarelen-1) - 1][2])
+        # temptlist.append([boundlist[squarelen*(squarelen-1) - 1][3][0],boundlist[squarelen*squarelen - 2][0][1]])
+        for x in range(squarelen):
+            temptlist = []
+            temptlist.append(boundlist[0][3])
+            temptlist.append(boundlist[x * squarelen][0])
+            temptlist.append([boundlist[x * squarelen][0][0],boundlist[x][2][1]])
+            temptlist.append(boundlist[x][2])
+            spacelist.append(temptlist)
 
-#         for x in range(squarelen - 1):
-#             temptlist = []
-#             temptlist.append(boundlist[squarelen*squarelen - 1][1])
-#             temptlist.append(boundlist[squarelen*squarelen - 1 - x][0])
-#             temptlist.append([boundlist[squarelen*(squarelen-x) - 1][3][0],boundlist[squarelen*squarelen - 1 - x][0][1]])
-#             temptlist.append(boundlist[squarelen*(squarelen-x) - 1][2])
-#             spacelist.append(temptlist)
+        for x in range(squarelen - 1):
+            temptlist = []
+            temptlist.append(boundlist[squarelen*squarelen - 1][1])
+            temptlist.append(boundlist[squarelen*squarelen - 1 - x][0])
+            temptlist.append([boundlist[squarelen*(squarelen-x) - 1][3][0],boundlist[squarelen*squarelen - 1 - x][0][1]])
+            temptlist.append(boundlist[squarelen*(squarelen-x) - 1][2])
+            spacelist.append(temptlist)
             
-#         valuelist=[]
-#         # print(boundlist[0])
-#         for x in boundlist:
-#             # print(x[4],x[5])
-#             if x[4] != 0:
-#                 # print(x[4])
-#                 valuelist.append(x)
-#         valuelist = sorted(valuelist,key=lambda x: x[5],reverse=True)
-# #         sortNum = 1
-#         newlist = []
-#         # print(valuelist[0])
-#         # print(valuelist)
-#         for x in valuelist:
-#             tempt = x[0:6]
-# #             tempt.append(sortNum)
-# #             sortNum += 1
-#             newlist.append(tempt)
-#         return spacelist,newlist,baseLocation
+        valuelist=[]
+        # print(boundlist[0])
+        for x in boundlist:
+            # print(x[4],x[5])
+            if x[4] != 0:
+                # print(x[4])
+                valuelist.append(x)
+        valuelist = sorted(valuelist,key=lambda x: x[5],reverse=True)
+#         sortNum = 1
+        newlist = []
+        # print(valuelist[0])
+        # print(valuelist)
+        for x in valuelist:
+            tempt = x[0:6]
+#             tempt.append(sortNum)
+#             sortNum += 1
+            newlist.append(tempt)
+        return spacelist,newlist,baseLocation
 
 
 user = 'james' 
 userlist = ['james'] 
 elFence = electricFence() 
 elFence.pullData(user,userlist,days=14)
-elFence.squareBounds() 
-# spacelist,valuelist,base = elFence.squareBounds() 
-# print(spacelist) 
+spacelist,valuelist,base = elFence.squareBounds() 
+print(spacelist) 
 # print(valuelist)
 # print(base)
